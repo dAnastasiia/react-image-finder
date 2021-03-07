703ddc139fd81b49b9834c65fc08fa9c3127b67c
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-03-07 22:47:18 +0200

    create component App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,91 @@
 import React, { Component } from 'react';
 
-// apiKey: '19670385-c29ff9f36201f4e7ced09e1e2'
-// baseUrl: 'https://pixabay.com/api/'
+import Section from '../Section';
+import Searchbar from '../Searchbar';
+import ImageGallery from '../ImageGallery';
+import Button from '../Button';
+import Loader from 'react-loader-spinner';
+import Modal from '../Modal';
 
-class App extends Component {
-  state = {};
+import imagesApi from '../../services/images-api';
+
+export default class App extends Component {
+  state = {
+    images: [],
+    currentPage: 1,
+    searchQuery: '',
+    showModal: false,
+    isLoading: false,
+    error: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.fetchImages();
+    }
+  }
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  onChangeQuery = query => {
+    this.setState({
+      searchQuery: query,
+      currentPage: 1,
+      images: [],
+      error: null,
+    });
+  };
+
+  fetchImages = () => {
+    const { searchQuery, currentPage } = this.state;
+
+    this.setState({ isLoading: true });
+
+    imagesApi(searchQuery, currentPage)
+      .then(data => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data],
+          currentPage: prevState.currentPage + 1,
+        }));
+
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ isLoading: false }));
+  };
 
   render() {
-    return <h1>Image Finder</h1>;
+    const { images, showModal, isLoading, hasError } = this.state;
+    const shouldRenderLoadMoreBtn = images.length > 0 && !isLoading;
+
+    return (
+      <Section>
+        {hasError && <h1>Something went wrong...</h1>}
+
+        <Searchbar onSubmit={this.onChangeQuery} />
+        <ImageGallery images={images} />
+
+        {shouldRenderLoadMoreBtn && <Button onClick={this.fetchImages} />}
+
+        {isLoading && (
+          <Loader
+            type="Puff"
+            color="#3f51b5"
+            height={50}
+            width={50}
+            timeout={3000}
+          />
+        )}
+
+        {showModal && <Modal onClose={this.toggleModal} />}
+      </Section>
+    );
   }
 }
-
-export default App;
