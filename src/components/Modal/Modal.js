1adce7084c487b5eb98a4711c5d9f6304f08f3e7
DrAnastasiia <dr.anastasiyya0@gmail.com>
2021-03-07 22:46:54 +0200

    create component Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+    // tags: PropTypes.string.isRequired,
+    // largeImageURL: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    // const { tags, largeImageURL } = this.props;
+    return createPortal(
+      <div className="Overlay" onClick={this.handleBackdropClick}>
+        <div className="Modal">
+          <img src="https://i.ytimg.com/vi/tOB8W0j_2qc/mqdefault.jpg" alt="" />
+        </div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
+
+export default Modal;
